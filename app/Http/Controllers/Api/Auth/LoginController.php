5478047866fdd7df069ee8ed730e862a60a24752
denis.krupin <krupin@machaon.ru>
2020-08-20 15:00:43 +0300

    Features: add auth

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'You cannot sign with those credentials',
+                'errors' => 'Unauthorised',
+            ], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $token = Auth::user()->createToken(config('app.name'));
+
+        $token->token->expires_ad =
+            $request->remember_me
+            ? Carbon::now()->addMonth()
+            : Carbon::now()->addDay();
+        $token->token->save();
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'token' => $token->accessToken,
+            'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString(),
+        ]);
+    }
+}
