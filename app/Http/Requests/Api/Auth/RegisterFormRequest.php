6db0106bb2c9c0eb0acc3a6afdf040315a1049aa
kashiuno <taigusiobaka@gmail.com>
2020-08-29 10:28:33 +0300

    Features: add messages to validation.

--- app/Http/Requests/Api/Auth/RegisterFormRequest.php
+++ app/Http/Requests/Api/Auth/RegisterFormRequest.php
@@ -29,4 +29,23 @@ class RegisterFormRequest extends FormRequest
             'password' => ['required', 'string', 'min:8', 'max:50', 'confirmed'],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Field name is required',
+            'name.string' => 'Field name mst be a string',
+            'name.max:50' => 'Field name max size is 50 characters',
+            'email.require' => 'Field email is required',
+            'email.string' => 'Field email must be a string',
+            'email.email' => 'Field email must be a valid email',
+            'email.max:255' => 'Field email max size is 255 characters',
+            'email.unique' => 'Field email must be an unique',
+            'password.required' => 'Field password is required',
+            'password.string' => 'Field password must be a string',
+            'password.min:8' => 'Field password must have min 8 characters',
+            'password.max:50' => 'Field password must have max 50 characters',
+            'password.confirmed' => 'Field password must have a valid confirmation',
+        ];
+    }
 }
