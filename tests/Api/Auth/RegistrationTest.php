f9bef25f2b53113a1484474ad2034adc22bd34d8
kashiuno <taigusiobaka@gmail.com>
2020-09-11 22:37:49 +0300

    Tests: create api tests for login, logout, register.

---
+++ tests/Api/Auth/RegistrationTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Api\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const TEST_VALID_NAME = 'taigusio';
+    private const TEST_VALID_EMAIL = 'taigus@gmail.com';
+    private const TEST_VALID_PASSWORD = 'd549769e';
+    private const TEST_VALID_PASSWORD_CONFIRMATION = 'd549769e';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        Artisan::call('passport:install');
+        Artisan::call('db:seed', ['--class' => 'UserSeeder']);
+    }
+
+    public function testShouldReturn_200ResponseCodeIfTryWasSuccess()
+    {
+        $credentials = [
+            'name' => self::TEST_VALID_NAME,
+            'email' => self::TEST_VALID_EMAIL,
+            'password' => self::TEST_VALID_PASSWORD,
+            'password_confirmation' => self::TEST_VALID_PASSWORD_CONFIRMATION,
+        ];
+
+        $this->makeRequest($credentials)
+            ->assertStatus(Response::HTTP_OK);
+    }
+
+    public function testShouldReturnSuccessFullMessageIfTryWasSuccess()
+    {
+        $credentials = [
+            'name' => self::TEST_VALID_NAME,
+            'email' => self::TEST_VALID_EMAIL,
+            'password' => self::TEST_VALID_PASSWORD,
+            'password_confirmation' => self::TEST_VALID_PASSWORD_CONFIRMATION,
+        ];
+
+        $this->makeRequest($credentials)
+            ->assertExactJson([
+                'message' => 'You were successfully registered. Use your email and password to sign in',
+            ]);
+    }
+
+    public function testShouldReturnErrorsAndMessagesIfTryWasUnsuccess()
+    {
+        $credentials = [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+            'password_confirmation' => '',
+        ];
+
+        $this->makeRequest($credentials)
+            ->assertJsonFragment([
+                'message' => 'The given data was invalid.'
+            ])
+            ->assertJsonFragment([
+                'errors' => [
+                    'name' => ['Field name is required'],
+                    'email' => ['Field email is required'],
+                    'password' => ['Field password is required'],
+                ]
+            ]);
+    }
+
+    private function makeRequest(array $credentials): TestResponse
+    {
+        return $this->json('POST', '/api/register', $credentials);
+    }
+}
