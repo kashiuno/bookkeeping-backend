c70d4dc57625945315d0f17dade6876ca83d4f67
kashiuno <taigusiobaka@gmail.com>
2020-09-20 21:33:49 +0300

    Features: make name ununique.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique();
+            $table->string('name', 50);
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->timestamps();
